e67515415672811b5230578bd67d55d873968c15
divyanshusingh520 <divyanshus784@gmail.com>
2022-06-09 00:00:26 -0400

    react

---
+++ src/component/Content.js
@@ -0,0 +1,151 @@
+import React from "react";
+
+
+function Content() {
+    return (
+      <>
+      <div className="main">
+        <div className="content">
+          <div className="dashboard1">
+          <img src='/images/dashboard.png' alt=""/>
+          <div className="dashboard">Dashboard</div>
+           
+          </div>
+          <div className="inventory1">
+            <img src='/images/inventory.png' alt=""/>
+          <div className="inventory">Inventory</div>
+          </div>
+          <div className="orders1">
+          <img src='/images/orders.png' alt=""/>
+          <div className="orders">Orders</div>
+          </div>
+          <div className="shipping1">
+          <img src='/images/shipping.png' alt=""/>
+          <div className="shipping">shipping</div>
+          </div>
+          <div className="channel1">
+          <img src='/images/channel.png' alt=""/>
+          <div className="channel">Channel</div>
+          </div>
+       </div>
+       <div className="content1">
+         <div className="upper"></div>
+          <div className="pending">Pending</div>
+          <div className="accepted">Accepted</div>
+          <div className="awbcreated">AWB Created</div>
+          <div className="readytoship">Ready to ship</div>
+          <div className="shipped">Shipped</div>
+          <div className="completed">completed</div>
+          <div className="cancelled">cancelled</div>
+        </div>
+      
+       
+
+      </div>
+      <div className="main1">
+        <div className="content">
+          
+       </div>
+       <div className="content2">
+         <div className="importorders1">
+          <div className="importorders">Import Orders</div>
+          </div>
+          <div className="accept1">
+          <div className="accept">Accept</div>
+          </div>
+          <div className="print1">
+          <div className="print">Print</div>
+          </div>
+        </div>
+      
+      
+       
+
+      </div>
+      {/* <div className="main">
+        <div className="content">
+          
+       </div> */}
+       <div className="content3">
+         <div className=""></div>
+          <div className="channel">Channel</div>
+          <div className="orderno">Order No</div>
+          <div className="orderdate">Order date</div>
+          <div className="city">City</div>
+          <div className="customername">Customer Name</div>
+          <div className="ordervalue">Order Value</div>
+          <div className="status">Status</div>
+          <div className="operation">operation</div>
+        </div>
+        
+       
+
+      {/* </div> */}
+      {/* <div className="main">
+        <div className="content">
+          
+       </div> */}
+       <div className="content4">
+         <div className="sign">+</div>
+          <div className="channel">$</div>
+          <div className="orderno">#TKN20203754</div>
+          <div className="orderdate">2022-05-04</div>
+          <div className="city">Lucknow</div>
+          <div className="customername">Abhishek Dixit</div>
+          <div className="ordervalue">0.00</div>
+          <div className="status">Pending</div>
+          <div className="operation">Action</div>
+        </div>
+        
+       
+
+      {/* </div> */}
+      {/* <div className="main">
+        <div className="content">
+          
+       </div> */}
+       <div className="content5">
+       <div className="sign">+</div>
+          <div className="channel">$</div>
+          <div className="orderno">#TKN20203753</div>
+          <div className="orderdate">2022-05-04</div>
+          <div className="city">Lucknow</div>
+          <div className="customername">Abhishek Dixit</div>
+          <div className="ordervalue">0.00</div>
+          <div className="status">Pending</div>
+          <div className="operation">Action</div>
+        </div>
+       
+
+      {/* </div> */}
+      {/* <div className="main">
+        <div className="content">
+          
+       </div> */}
+       <div className="content6">
+       <div className="sign">+</div>
+          <div className="channel">$</div>
+          <div className="orderno">#TKN20203752</div>
+          <div className="orderdate">2022-05-04</div>
+          <div className="city">Lucknow</div>
+          <div className="customername">Abhishek Dixit</div>
+          <div className="ordervalue">0.00</div>
+          <div className="status">Pending</div>
+          <div className="operation">Action</div>
+        </div>
+      {/* </div> */}
+      <div className="footer">
+
+      </div>
+      <div className="footerdown">
+        
+      </div>
+      
+      
+      
+      </>
+    );
+  }
+  
+  export default Content;
+  
